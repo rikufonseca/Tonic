4f1efb6bd772eb727db7d6a1b19d98f55a45c13b
erika fonseca <fonsecarika@gmail.com>
2023-02-02 21:10:56 +0200

    routes ok

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-   resources :jewelries, only: %i(index new create edit update destroy)
-   resources :products, only: %i(index new create edit update destroy)
-   resources :services, only: %i(index new create edit update destroy)
+   resources :jewelries, only: %i(index show new create edit update destroy)
+   resources :products, only: %i(index show new create edit update destroy)
+   resources :services, only: %i(index show new create edit update destroy)
    resources :contacts, only: %i(create)
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
