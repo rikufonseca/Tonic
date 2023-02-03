1c15f1df9391ac60dcd6b9fbe1acffe39cd2494b
erika fonseca <fonsecarika@gmail.com>
2023-02-03 20:24:39 +0200

    active_admin done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_03_131514) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_03_181511) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -89,6 +89,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_03_131514) do
     t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "time"
   end
 
   create_table "services", force: :cascade do |t|
@@ -101,6 +102,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_03_131514) do
     t.string "category_gr"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "time"
   end
 
   create_table "users", force: :cascade do |t|
