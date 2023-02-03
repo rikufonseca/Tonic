e5b403fd7a4bbf57fae12ef65ac5b082ee098b48
erika fonseca <fonsecarika@gmail.com>
2023-02-03 13:20:57 +0200

    validations done

--- app/models/jewelry.rb
+++ app/models/jewelry.rb
@@ -1,2 +1,4 @@
 class Jewelry < ApplicationRecord
+  has_many_attached :photos
+  validates :theme_gr, :theme_en, :name_gr, :name_en, :category_en, :category_gr, :description_gr, :description_en, :price, presence: true
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,4 @@
 class Product < ApplicationRecord
+  has_one_attached :photo
+  validates :name_gr, :name_en, :description_gr, :description_en, :price, presence: true
 end
