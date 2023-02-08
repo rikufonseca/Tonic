fa9c89f32c8363bb639b08259f98060a18f3597b
erika fonseca <fonsecarika@gmail.com>
2023-02-08 13:54:49 +0200

    home_page and gsap installed

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -7,3 +7,9 @@ application.debug = false
 window.Stimulus   = application
 
 export { application }
+
+import { navbar } from "./navbar";
+
+document.addEventListener('turbolinks:load', () => {
+  navbar();
+});
