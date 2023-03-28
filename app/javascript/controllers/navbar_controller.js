ea656048afc5f2f781ceb7aa4e239e07868093c7
erika fonseca <fonsecarika@gmail.com>
2023-03-28 17:27:06 +0300

    in progress

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -1,61 +1,76 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["navigation", "homepage", "upbar", "textbar", "navtel", "logo"];
+  static targets = ["navigation", "homepage", "upbar", "textbar", "navtel", "logo" ];
 
   connect() {
 
-    const navigation = this.navigationTarget;
+    const navigation = this.navigationTargets;
     const homepage = this.homepageTarget;
-    const upbar = this.upbarTarget;
-    const textbar = this.textbarTarget;
+    const upbar = this.upbarTargets;
+    console.log(upbar);
+    const textbar = this.textbarTargets;
+    console.log(textbar);
     const navtel = this.navtelTarget;
     const logo = this.logoTarget;
-    console.log(logo);
 
-    if (window.matchMedia("(min-width:1000px)").matches) {
+    if( window.matchMedia("(min-width:1000px)").matches){
       function updatePosition() {
         let rect = homepage.getBoundingClientRect();
         var top = rect.top;
-        console.log(top);
+
         if(top <= 0){
-          navigation.classList.add("seen");
-          navigation.classList.add("move-down");
-          upbar.classList.add("move-down-call");
-          textbar.classList.add("show-call");
+          navigation[0].classList.add("seen");
+          navigation[0].classList.add("move-down");
+          upbar[0].classList.add("move-down-call");
+          textbar[0].classList.add("show-call");
         } else {
-          navigation.classList.remove("seen");
-          navigation.classList.remove("move-down");
-          upbar.classList.remove("move-down-call");
-          textbar.classList.remove("show-call");
+          navigation[0].classList.remove("seen");
+          navigation[0].classList.remove("move-down");
+          upbar[0].classList.remove("move-down-call");
+          textbar[0].classList.remove("show-call");
         }
       }
       window.addEventListener("scroll", updatePosition);
     }
 
-    if (window.matchMedia("(max-width:1000px)").matches) {
+    if(window.matchMedia("(max-width:1000px)").matches) {
 
-      function updatePosition() {
+      function updatePositionWithMenuOpen() {
         let rect = homepage.getBoundingClientRect();
-        var top = rect.top;
+        const top = rect.top;
         console.log(top);
-        if (top <= 203) {
-          if (navtel.classList.contains("show-call")){
-              navtel.style.top = "96px";
-              navtel.classList.add("seen");
-              logo.style.marginTop = "-128px"
-          console.log("now");}
 
+        if (top <= 203 && navtel.classList.contains("show-call")){
+          navtel.style.top = "0px";
+          navtel.classList.add("seen");
+          logo.style.marginTop = "-138px"
+          console.log("not ok")
+        } else if (top <= 97 && !navtel.classList.contains("show-call")){
+          navigation[1].classList.add("seen_tel");
+          navigation[1].style.top = "53px";
+          navigation[1].classList.add("move-down-tel");
+          upbar[1].classList.add("move-down-call");
+          textbar[1].classList.add("show-call");
+          console.log("ok");
+        } else if (top >= 0 && navigation[1].classList.contains("seen_tel")){
+          navigation[1].classList.remove("seen_tel");
+          navigation[1].style.top = "0px";
+          navigation[1].classList.remove("move-down-tel");
+          upbar[1].classList.remove("move-down-call");
+          textbar[1].classList.remove("show-call");
+        } else {
+          console.log("thv gkamises")
         }
       }
-      window.addEventListener("scroll", updatePosition);
+      window.addEventListener("scroll", updatePositionWithMenuOpen);
     }
   }
 
   displayMenu() {
     const navtel = this.navtelTarget;
 
-    if (navtel.classList.contains("show-call") ) {
+    if(navtel.classList.contains("show-call") ) {
       navtel.classList.add("fade-out");
       navtel.classList.remove("show-call");
     } else {
