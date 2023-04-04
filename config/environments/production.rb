46c0ebc77d906b1925594ac3b189bd7a3a55ee4a
erika fonseca <fonsecarika@gmail.com>
2023-04-04 12:25:27 +0300

    ok

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,13 +2,13 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: "http://tonic-society.com" }
+  config.action_mailer.default_url_options = { host: "https://tonic-prod.herokuapp.com/" }
   # Settings specified here will take precedence over those in config/application.rb.
   config.action_mailer.smtp_settings = {
-    address: ENV['SMTP_SERVER'],
-    port: ENV['SMTP_PORT'],
-    user_name: ENV['SMTP_USER_NAME'],
-    password: ENV['SMTP_PASSWORD'],
+    address: ENV('SMTP_SERVER'),
+    port: ENV('SMTP_PORT'),
+    user_name: ENV('SMTP_USER_NAME'),
+    password: ENV('SMTP_PASSWORD'),
     authentication: :plain,
     enable_starttls_auto: true
   }
@@ -63,7 +63,7 @@ Rails.application.configure do
   config.log_level = :info
 
   # Prepend all log lines with the following tags.
-  config.log_tags = [ :request_id ]
+  config.log_tags = [:request_id]
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
