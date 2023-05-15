e95c865138293be3fab75fd43b78269b5a8ac4d3
erika fonseca <fonsecarika@gmail.com>
2023-05-15 19:57:59 +0300

    in progress page services

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -19,7 +19,7 @@ export default class extends Controller {
         });
         rects.forEach((rect) => {
           const top = rect.top;
-          if(top <= 0){
+          if(top < 0){
             navigation[0].classList.add("seen");
             navigation[0].classList.add("move-down");
             upbar[0].classList.add("move-down-call");
@@ -46,8 +46,9 @@ export default class extends Controller {
         });
         rects.forEach((rect) => {
           const top = rect.top;
-          console.log(top)
+          console.log(top);
           if (top <= 203 && navtel.style.opacity === "1"){
+            console.log("in-one");
             navtel.classList.remove("display-menu");
             navtel.style.top = "-33px";
             upbar[1].classList.remove("move-down-call");
@@ -55,6 +56,7 @@ export default class extends Controller {
             navigation[1].style.opacity = "0";
             navigation[1].style.top = "0px";
           } else if (top <= 97 && navtel.classList.contains("opacity")){
+            console.log("in-two");
             navtel.classList.remove("display-menu");
             navigation[1].classList.add("seen_tel");
             navigation[1].style.top = "25px";
@@ -64,6 +66,7 @@ export default class extends Controller {
             textbar[1].classList.add("show-call");
           } else {
               if(navtel.style.opacity === "1"){
+                console.log("in-three");
                 navtel.classList.remove("display-menu");
                 navtel.style.backgroundColor = "rgba(222, 208, 199,0)"
                 upbar[1].classList.remove("move-down-call");
@@ -72,6 +75,7 @@ export default class extends Controller {
                 navigation[1].style.top = "0px";
                 navtel.style.top = "112px";
               }
+            console.log("in-four");
               navtel.classList.remove("display-menu");
               upbar[1].classList.remove("move-down-call");
               navtel.style.top = "-15px";
@@ -87,7 +91,7 @@ export default class extends Controller {
   }
 
   displayMenu(event) {
-    console.log("in")
+  //  console.log("in")
     event.preventDefault();
     const links = this.element.querySelectorAll('a');
     const navtel = this.navtelTarget;
@@ -116,17 +120,17 @@ export default class extends Controller {
       const top = rect.top;
       let rectNav = navigation[1].getBoundingClientRect();
       const topNav = rectNav.top;
-      console.log(topNav)
+    //  console.log(topNav)
 
       if ((topNav === 0) && (top >= 267)) {
-        console.log("112px")
+      //  console.log("112px")
         if (window.location.pathname === "/en/contacts" || window.location.pathname === "/gr/contacts" ) {
           navtel.style.backgroundColor = "rgba(244, 244, 244,1)";
         } else {
           navtel.style.backgroundColor = "rgba(222, 208, 199,0)";
         }
         if(navtel.classList.contains("opacity")){
-          console.log("in-one")
+      //    console.log("in-one")
           navtel.classList.remove("display-menu");
           navtel.style.opacity = "1";
           navtel.style.top = '112px';
@@ -134,7 +138,7 @@ export default class extends Controller {
           navtel.classList.remove("opacity");
           links.forEach(link => { link.style.pointerEvents = "auto" })
         } else {
-          console.log("in-two")
+      //    console.log("in-two")
           navtel.classList.remove("display-menu");
           navtel.style.opacity = "0";
           navtel.style.top = '0px';
@@ -143,10 +147,10 @@ export default class extends Controller {
           links.forEach(link => { link.style.pointerEvents = "none" })
         }
       } else {
-        console.log("137px")
+    //    console.log("137px")
         navtel.style.backgroundColor = "#ded0c7"
         if (navtel.classList.contains("show-call")) {
-          console.log("in-three")
+      //    console.log("in-three")
           navtel.classList.remove("display-menu");
           navtel.style.top = '-15px';
           navtel.classList.add("seen_tel");
@@ -155,7 +159,7 @@ export default class extends Controller {
           navtel.classList.add("opacity");
           links.forEach(link => { link.style.pointerEvents = "none" })
         } else {
-          console.log("in-four")
+      //    console.log("in-four")
           navtel.classList.remove("display-menu");
           navtel.style.top = '137px';
           navtel.style.opacity = "1";
