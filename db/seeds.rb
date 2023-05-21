a38150df5cd1027e0984b2c9704ca2f45c08f7cb
Loris Reynaud <loris.reynaud@gmail.com>
2023-05-21 14:30:10 +0200

    seeds updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,25 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-puts "Seeds start"
+puts "Seeds start..."
+puts ""
+
+puts "destroy datas"
 User.destroy_all if Rails.env.development?
-Services.destroy_all if Rails.env.development?
+Service.destroy_all if Rails.env.development?
+puts "Datas destroyed"
+puts ""
 
-
+puts "creating users..."
 User.create!(
   email: "fonsecarika@gmail.com", password: "1234567890!6", admin: true
 )
+puts "Boss created"
 
-puts "user created"
+User.create!(
+  email: "loris.reynauda@gmail.com", password: "azerty", admin: true
+)
+puts "Loris created"
+puts ""
+
+puts "Seeds successfully finished !"
