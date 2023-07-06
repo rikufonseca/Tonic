471d651cae85d2155d5274ddc0706f394c83477a
Loris Reynaud <loris.reynaud@gmail.com>
2023-07-06 20:51:31 +0300

    services meta-datas

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -20,12 +20,18 @@ class PagesController < ApplicationController
   private
 
   def og_params
+    en_desc = "At Tonic, we believe in creating an unparalleled Nail Salon experience, 
+    where every detail is meticulously crafted to provide you with a moment of pure indulgence."
+    gr_desc = "xsdfghjgklyjreraz"
+
+    en_title = "Tonic - WHO WE ARE?"
+    gr_title = ""
+
     {
-      title: "Tonic - WHO WE ARE?",
+      title: request.original_url.include?("gr") ? gr_title : en_title,
       type: "website",
       image: "https://www.tonic-society.com/assets/home_background-7176d7033963e50dc91ecc3c193c764a2afb3d0a2164caa6f6bf2037348ca2fc.png",
-      description: "At Tonic, we believe in creating an unparalleled Nail Salon experience,
-        where every detail is meticulously crafted to provide you with a moment of pure indulgence.",
+      description: request.original_url.include?("gr") ? gr_desc : en_desc,
       site_name: "Tonic - Nails & Jewelry",
       alternate: { "en" => "https://tonic-society.com/en", "gr" => "https://tonic-society.com/gr" }
     }
--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -2,15 +2,39 @@ class ServicesController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
+    en_desc = "Nails - Manucure and pedicure menu, for men & women"
+    gr_desc = "xsdfghjgklyjreraz"
+
+    en_title = "Nails Menu"
+    gr_title = ""
+
     set_meta_tags(
-      title: "Nails Menu",
-      description: "All the nails sevices, manucure / pedicure for men and women",
-      keywords: "tonic, luxury, nails, jewelry, kolonaki, athens, greece",
+      title: request.original_url.include?("gr") ? en_title : gr_title,
+      description: request.original_url.include?("gr") ? gr_desc : en_desc,
       reverse: true,
-      og: og_params
+      og: og_index_params
     )
   end
 
   def show
+  end
+
+  private
+
+  def og_index_params
+    en_desc = "Nails - Manucure and pedicure menu, for men & women"
+    gr_desc = "xsdfghjgklyjreraz"
+
+    en_title = "Tonic - Nails Menu"
+    gr_title = ""
+
+    {
+      title: request.original_url.include?("gr") ? gr_title : en_title,
+      type: "website",
+      image: "https://www.tonic-society.com/assets/home_background-7176d7033963e50dc91ecc3c193c764a2afb3d0a2164caa6f6bf2037348ca2fc.png",
+      description: request.original_url.include?("gr") ? gr_desc : en_desc,
+      site_name: "Tonic - Nails & Jewelry",
+      alternate: { "en" => "https://tonic-society.com/en", "gr" => "https://tonic-society.com/gr" }
+    }
   end
 end
