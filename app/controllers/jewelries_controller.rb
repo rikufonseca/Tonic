a758326022535d1cae61ab8bb8b23af653a47c42
erika fonseca <fonsecarika@gmail.com>
2024-04-09 15:41:33 +0300

    small refacto

--- app/controllers/jewelries_controller.rb
+++ app/controllers/jewelries_controller.rb
@@ -1,13 +1,14 @@
 class JewelriesController < ApplicationController
   skip_before_action :authenticate_user!
+   before_action :all, only: %i[index all_themes_all_cat]
 
   def index
-    @jewelries = Jewelry.all
-    if params[:category]
+     if params[:category]
       @jewelries = @jewelries.where(category_en: params[:category])
     elsif params[:theme]
       @jewelries = @jewelries.where(theme_en: params[:theme])
     end
+
     jewelries_giampouras = @jewelries.where(artist_id: Artist.find_by(name_en: "Vasilis Giampouras"))
     jewelries_kanale = @jewelries.where(artist_id: Artist.find_by(name_en: "Maria Kanala"))
     @fashionables = jewelries_giampouras.where(theme_en: "Fashionable")
@@ -16,9 +17,16 @@ class JewelriesController < ApplicationController
     @lava_stone = jewelries_kanale.where(theme_en: "Lava Stone")
     @faith = jewelries_kanale.where(theme_en: "Faith")
     @flowers_kanale = jewelries_kanale.where(theme_en: "Flowers")
+    all_themes_all_cat
+  end
 
+  def all_themes_all_cat
     @themes_en = Jewelry.pluck(:theme_en).uniq
     @categories_en = Jewelry.pluck(:category_en).uniq
+  end
+
+  def all
+    @jewelries = Jewelry.all
   end
 
   def show
