fe5c74e52bf939ecf8f68b14648722147be1f766
erika fonseca <fonsecarika@gmail.com>
2024-04-09 15:33:18 +0300

    filtre active

--- app/controllers/jewelries_controller.rb
+++ app/controllers/jewelries_controller.rb
@@ -3,6 +3,11 @@ class JewelriesController < ApplicationController
 
   def index
     @jewelries = Jewelry.all
+    if params[:category]
+      @jewelries = @jewelries.where(category_en: params[:category])
+    elsif params[:theme]
+      @jewelries = @jewelries.where(theme_en: params[:theme])
+    end
     jewelries_giampouras = @jewelries.where(artist_id: Artist.find_by(name_en: "Vasilis Giampouras"))
     jewelries_kanale = @jewelries.where(artist_id: Artist.find_by(name_en: "Maria Kanala"))
     @fashionables = jewelries_giampouras.where(theme_en: "Fashionable")
@@ -12,12 +17,8 @@ class JewelriesController < ApplicationController
     @faith = jewelries_kanale.where(theme_en: "Faith")
     @flowers_kanale = jewelries_kanale.where(theme_en: "Flowers")
 
-    @all_theme = []
-    @all_category = []
-    @jewelries.each do |jewelry|
-      @all_theme << jewelry.theme_en
-      @all_category << jewelry.category_en
-    end
+    @themes_en = Jewelry.pluck(:theme_en).uniq
+    @categories_en = Jewelry.pluck(:category_en).uniq
   end
 
   def show
